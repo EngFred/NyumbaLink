57e73bcee49e3b8d9c0dea2f6a3d229c617d2c3e
Engineer Fred <engfred88@gmail.com>
2026-05-19 20:17:20 +0300

    updated

--- lib/features/hostel-alerts/data/datasources/hostel_alerts_remote_datasource.dart
+++ lib/features/hostel-alerts/data/datasources/hostel_alerts_remote_datasource.dart
@@ -1,8 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:rentora/features/hostel-alerts/domain/entities/hostel_alert.dart';
 
 import '../../../../../core/network/dio_client.dart';
+import '../models/hostel_alert_model.dart';
 
 final hostelAlertsRemoteDataSourceProvider =
     Provider<HostelAlertsRemoteDataSource>(
@@ -13,35 +13,23 @@ class HostelAlertsRemoteDataSource {
   const HostelAlertsRemoteDataSource(this._dio);
   final Dio _dio;
 
-  Future<HostelAlert> subscribe(String propertyId) async {
+  Future<HostelAlertModel> subscribe(String propertyId) async {
     final res = await _dio.post<Map<String, dynamic>>(
       '/user-hostel-alerts',
       data: {'propertyId': propertyId},
     );
-    return _fromJson(res.data!);
+    return HostelAlertModel.fromJson(res.data!);
   }
 
   Future<void> unsubscribe(String propertyId) async {
     await _dio.delete('/user-hostel-alerts/$propertyId');
   }
 
-  Future<List<HostelAlert>> getMyAlerts() async {
+  Future<List<HostelAlertModel>> getMyAlerts() async {
     final res = await _dio.get<List<dynamic>>('/user-hostel-alerts/my');
     return (res.data ?? [])
         .cast<Map<String, dynamic>>()
-        .map(_fromJson)
+        .map(HostelAlertModel.fromJson)
         .toList();
-  }
-
-  HostelAlert _fromJson(Map<String, dynamic> json) {
-    final property = json['property'] as Map<String, dynamic>? ?? {};
-    return HostelAlert(
-      id: json['id'] as String,
-      propertyId: json['propertyId'] as String,
-      propertyTitle: property['title'] as String? ?? '',
-      createdAt:
-          DateTime.tryParse(json['createdAt'] as String? ?? '') ??
-          DateTime.now(),
-    );
   }
 }
